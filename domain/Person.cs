0f119ca94a91858bc9022144bf3dbfed12d0e17a
Ahmed Agabani <agabani@live.com>
2017-02-06 23:43:22 +0000

    reorder

--- domain/Person.cs
+++ domain/Person.cs
@@ -17,78 +17,9 @@ namespace domain
 
     public partial class Person
     {
-        private static readonly IReadOnlyDictionary<Type, Action<Event, Person>> Actions;
-
-        static Person()
-        {
-            Actions = new Dictionary<Type, Action<Event, Person>>
-            {
-                {typeof(PersonBornEvent), PersonBornEvent},
-                {typeof(PersonNamedEvent), PersonNamedEvent},
-                {typeof(PersonStartedEductionEvent), PersonStartedEducationEvent},
-                {typeof(PersonFinishedEducationEvent), PersonFinishedEducationEvent},
-                {typeof(PersonStartedExperienceEvent), PersonStartedExperienceEvent},
-                {typeof(PersonFinishedExperienceEvent), PersonFinishedExperienceEvent}
-            };
-        }
-
-        private static void PersonBornEvent(Event @event, Person person)
-        {
-            var personBornEvent = (PersonBornEvent) @event;
-            person.Gender = personBornEvent.Gender;
-            person.DateOfBirth = personBornEvent.DateOfBirth;
-        }
-
-        private static void PersonNamedEvent(Event @event, Person person)
-        {
-            var personNamedEvent = (PersonNamedEvent) @event;
-            person.FirstName = personNamedEvent.FirstName;
-            person.LastName = personNamedEvent.LastName;
-        }
-
-        private static void PersonStartedEducationEvent(Event @event, Person person)
-        {
-            var personStartedEductionEvent = (PersonStartedEductionEvent) @event;
-            person.EducationalHistory.Add(new Education
-            {
-                InstitutionName = personStartedEductionEvent.InstitutionName,
-                StartDate = personStartedEductionEvent.When
-            });
-        }
-
-        private static void PersonFinishedEducationEvent(Event @event, Person person)
-        {
-            var personFinishedEducationEvent = (PersonFinishedEducationEvent) @event;
-            person.EducationalHistory
-                .Single(e => e.InstitutionName == personFinishedEducationEvent.InstitutionName
-                             && e.EndDate == null)
-                .EndDate = @event.When;
-        }
-
-        private static void PersonStartedExperienceEvent(Event @event, Person person)
-        {
-            var personStartedExperienceEvent = (PersonStartedExperienceEvent) @event;
-            person.ExperienceHistory.Add(new Experience
-            {
-                InstitutionName = personStartedExperienceEvent.InstitutionName,
-                Title = personStartedExperienceEvent.Title,
-                StartDate = personStartedExperienceEvent.When
-            });
-        }
-
-        private static void PersonFinishedExperienceEvent(Event @event, Person person)
-        {
-            var personFinishedExperienceEvent = (PersonFinishedExperienceEvent) @event;
-            person.ExperienceHistory
-                .Single(e => e.InstitutionName == personFinishedExperienceEvent.InstitutionName
-                             && e.Title == personFinishedExperienceEvent.Title
-                             && e.EndDate == null)
-                .EndDate = personFinishedExperienceEvent.When;
-        }
-
         public void Apply(Event @event)
         {
-            Actions[@event.GetType()].Invoke(@event, this);
+            Actions[@event.GetType()].Invoke(this, @event);
         }
 
         public static Person LoadFrom(List<Event> events)
@@ -99,6 +30,78 @@ namespace domain
                 person.Apply(@event);
             }
             return person;
+        }
+    }
+
+    public partial class Person
+    {
+        private static readonly IReadOnlyDictionary<Type, Action<Person, Event>> Actions;
+
+        static Person()
+        {
+            Actions = new Dictionary<Type, Action<Person, Event>>
+            {
+                {typeof(PersonBornEvent), PersonBornEvent},
+                {typeof(PersonNamedEvent), PersonNamedEvent},
+                {typeof(PersonStartedEductionEvent), PersonStartedEducationEvent},
+                {typeof(PersonFinishedEducationEvent), PersonFinishedEducationEvent},
+                {typeof(PersonStartedExperienceEvent), PersonStartedExperienceEvent},
+                {typeof(PersonFinishedExperienceEvent), PersonFinishedExperienceEvent}
+            };
+        }
+
+        private static void PersonBornEvent(Person person, Event @event)
+        {
+            var personBornEvent = (PersonBornEvent) @event;
+            person.Gender = personBornEvent.Gender;
+            person.DateOfBirth = personBornEvent.DateOfBirth;
+        }
+
+        private static void PersonNamedEvent(Person person, Event @event)
+        {
+            var personNamedEvent = (PersonNamedEvent) @event;
+            person.FirstName = personNamedEvent.FirstName;
+            person.LastName = personNamedEvent.LastName;
+        }
+
+        private static void PersonStartedEducationEvent(Person person, Event @event)
+        {
+            var personStartedEductionEvent = (PersonStartedEductionEvent) @event;
+            person.EducationalHistory.Add(new Education
+            {
+                InstitutionName = personStartedEductionEvent.InstitutionName,
+                StartDate = personStartedEductionEvent.When
+            });
+        }
+
+        private static void PersonFinishedEducationEvent(Person person, Event @event)
+        {
+            var personFinishedEducationEvent = (PersonFinishedEducationEvent) @event;
+            person.EducationalHistory
+                .Single(e => e.InstitutionName == personFinishedEducationEvent.InstitutionName
+                             && e.EndDate == null)
+                .EndDate = @event.When;
+        }
+
+        private static void PersonStartedExperienceEvent(Person person, Event @event)
+        {
+            var personStartedExperienceEvent = (PersonStartedExperienceEvent) @event;
+            person.ExperienceHistory.Add(new Experience
+            {
+                InstitutionName = personStartedExperienceEvent.InstitutionName,
+                Title = personStartedExperienceEvent.Title,
+                StartDate = personStartedExperienceEvent.When
+            });
+        }
+
+        private static void PersonFinishedExperienceEvent(Person person, Event @event)
+        {
+            var personFinishedExperienceEvent = (PersonFinishedExperienceEvent) @event;
+            person.ExperienceHistory
+                .Single(e => e.InstitutionName == personFinishedExperienceEvent.InstitutionName
+                             && e.Title == personFinishedExperienceEvent.Title
+                             && e.EndDate == null)
+                .EndDate = personFinishedExperienceEvent.When;
         }
     }
 }
